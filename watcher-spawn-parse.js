863e3339a1f55947769097359deb356adea99a18
JollyDowner <tilldomke@gmail.com>
2019-02-22 15:44:48 +0100

    Added default values for child process commands

--- watcher-spawn-parse.js
+++ watcher-spawn-parse.js
@@ -1,15 +1,14 @@
+/** Script that watches specified file in same directory for changes and shows its access rights, size and name **/
+
 'use strict'
 const fs = require('fs');
 
 const filename = process.argv[2];
 const work_directory = __dirname;
+
 const spawn = require("child_process").spawn;
-const child_process_command = process.argv[3];
-
-const options = process.argv.slice(3-(process.argv.length-1));
-
-console.log(child_process_command);
-console.log(options);
+const child_process_command = process.argv[3] ? process.argv[3] : "ls"; //ls command as default
+const options = process.argv.length > 4 ? process.argv.slice(3-(process.argv.length-1)) : ["-lh"]; // -lh option as default if not more specified
 
 
 if (!filename){
@@ -17,17 +16,24 @@ if (!filename){
 }
 
 
-fs.access(work_directory + "/" + filename,fs.constants.R_OK,(err) => {
+fs.access(work_directory + "/" + filename,fs.constants.R_OK,(err) => { //Check for read access for the file
 
   if (err){
     throw err;
   }
 
   fs.watch(work_directory + "/" + filename,() => {
-    const ls = spawn("ls",["-l","-h",filename]);
+
+    options.push(filename);
+
+    const ls = spawn(child_process_command,options);
     let output = "";
 
-    ls.stdout.on("data",chunk => output+=chunk);
+    ls.stderr.on("error", (err) => {
+      ls.stderr.write(err.message);
+    });
+
+    ls.stdout.on("data",chunk => output+=chunk); //append stdout stream data
 
     ls.on("close", () => {
       const parts = output.split(/\s+/); // split on one or more whitespaces
